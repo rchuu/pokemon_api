1b8fc36a4e7f7b491dffaa1cb449ee4d3bd52582
Richard Chu <rchu1211@gmail.com>
2022-06-13 15:21:52 -0700

    without button display

--- src/components/PokemonComponent.js
+++ src/components/PokemonComponent.js
@@ -3,39 +3,22 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
 const PokemonComponent = (props) => {
-    let [state, setState] = useState([]);
-    let [count, setCount] = useState(0);
-
-    useEffect(() => { // useEffect is a hook that runs after the component is rendered
-        if (count > 0) {
-            axios.get('https://pokeapi.co/api/v2/pokemon/?limit=807')
-                .then(response => {
-                    state = response.data.results; // set the state to the data from the response
-                    setState([...state])
-                }
-                );
-            console.log(state);
-        }
-    }, [count])
-
-    const onClickHandler = () => {
-        console.log(state);
-        if (count === 0) {
-            setCount(1);
-        }
-        else {
-            setCount(0);
-            setState([]);
-        }
-        console.log(count);
-    };
+    const [listOfPokemons, setListOfPokemons] = useState([])
+    useEffect(() => {
+        axios
+            .get('https://pokeapi.co/api/v2/pokemon?limit=807')
+            .then(response => {
+                setListOfPokemons(response.data.results)
+                console.log(response.data.results)
+            })
+            .catch(err => console.log(err))
+    }, [])
 
     return (
         <div>
-            <button onClick={onClickHandler}>{count === 0 ? 'I got yo asses' : 'Go away'} </button>
-            {state.map((item, index) => {
-                return (<div key={index}>{item.name}</div>)
-            })}
+            {listOfPokemons.map((item, index) => (
+                <li key={index}> {item.name}</li>
+            ))}
         </div>
     )
 }
